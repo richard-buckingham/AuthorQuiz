a31c632537beb8878317fdf37bdf7f24bc91d819
richie.buckingham <richie.buckingham@gmail.com>
2018-12-22 11:08:18 +1100

    inital commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./AuthorQuiz.css";
+import "./bootstrap.min.css";
+
+class AuthorQuiz extends Component {
+  render() {
+    return <div>Author Quiz</div>;
+  }
+}
+
+export default AuthorQuiz;
