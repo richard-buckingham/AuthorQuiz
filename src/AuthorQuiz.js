3c3d26700473698d237c70595702a6da17c5d458
richie.buckingham <richie.buckingham@gmail.com>
2018-12-23 17:24:20 +1100

    add enzyme

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./AuthorQuiz.css";
 import "./bootstrap.min.css";
 
 class AuthorQuiz extends Component {
   render() {
-    return <div>Author Quiz</div>;
+    return <div>Author Quiz dude...</div>;
   }
 }
 
